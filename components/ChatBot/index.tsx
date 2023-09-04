c2c4e9fd51bc9a99e759916074be5238fc0499d6
Pedro Cruz <pedropcruz@outlook.pt>
2023-09-04 20:29:35 +0100

    feat(Chatbot): Add logic to on chatbot and styles

--- components/ChatBot/index.tsx
+++ components/ChatBot/index.tsx
@@ -1,17 +1,20 @@
-import { Dispatch, SetStateAction, useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import Image from "next/image"
+
 import { MdClose } from "react-icons/md"
 
-import messages from "@/lib/messages"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 
 import { Button } from "../ui/button"
+
+import messages from "@/lib/messages"
+
 import chatbot from "./styles"
 
 const {
@@ -21,62 +24,125 @@ const {
   closeBtnIcon,
   cardTitle,
   cardContent,
-  dateTimeLabel,
   botContentMessage,
   botName,
   botImageContainer,
   botImage,
   botMessage,
+  dateTimeLabel,
+  buttonGroup,
 } = chatbot()
 
 interface IChatBotProps {
   setOpen: Dispatch<SetStateAction<boolean>>
+  isOpen: boolean;
   dateTime: string
 }
 
 export const ChatBot = ({ setOpen, dateTime }: IChatBotProps) => {
-  return (
-    <Card className={card()}>
-      <CardHeader className={cardHeader()}>
-        <Avatar>
-          <AvatarImage src="/me.png" alt="chatbotimg" />
-          <AvatarFallback>PC</AvatarFallback>
-        </Avatar>
-        <div>
-          <CardTitle className={cardTitle()}>Pedro Cruz</CardTitle>
-        </div>
-        <Button
-          variant="outline"
-          className={closeBtn()}
-          onClick={() => setOpen(false)}
-        >
-          <MdClose className={closeBtnIcon()} />
-        </Button>
-      </CardHeader>
-      <CardContent className={cardContent()}>
-        <span className={dateTimeLabel()}>{dateTime}</span>
-        {messages.map(({ message, trigger, options, end }, index) => {
-          return (
-            <div className={botContentMessage()} key={index}>
-              <div className={botName()}>
-                <Avatar className={botImageContainer()}>
-                  <AvatarImage
-                    className={botImage()}
-                    src="/me.png"
-                    alt="chatbotimg"
-                  />
-                  <AvatarFallback>PC</AvatarFallback>
-                </Avatar>
-                <p>Pedro Cruz Assistant</p>
+  const delayBetweenMessages = 1000
+  const [isLoading, setIsLoading] = useState(false)
+  const [currentBotResponseIndex, setCurrentBotResponseIndex] = useState(0)
+  
+  const currentMessage = messages[currentBotResponseIndex]
+  const hasOptions = currentMessage.options?.length
+  const hasNextMessage = currentMessage.trigger
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const timeout = setTimeout(() => {
+      if ( !hasOptions && hasNextMessage !== undefined) {
+        setCurrentBotResponseIndex(currentBotResponseIndex + 1)
+      } 
+        setIsLoading(false)
+      
+    }, delayBetweenMessages)
+    
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [currentBotResponseIndex, hasOptions, hasNextMessage]);
+
+
+  const handleButtonClick = (trigger: string) => {
+    if (!isLoading) {
+      const nextMessageIndex = messages.findIndex((message) => message.id === trigger)
+
+      if (nextMessageIndex !== -1) {
+        setIsLoading(true)
+        setCurrentBotResponseIndex(nextMessageIndex)
+        setIsLoading(false)
+      }
+    }
+  }
+
+  const renderMessages = () => (
+    messages.map(({ message, options }, index) => {
+     if (index > currentBotResponseIndex) return null;
+    
+    const isCurrentMessage = index <= currentBotResponseIndex;
+    const shouldShowMessage = isCurrentMessage || (options && options.length > 0);
+
+      return (
+        <div className={botContentMessage()} key={index}>
+          <div className={botName()}>
+            <Avatar className={botImageContainer()}>
+              <AvatarImage
+                className={botImage()}
+                src="/me.png"
+                alt="chatbotimg"
+              />
+              <AvatarFallback>PC</AvatarFallback>
+            </Avatar>
+            <p>Pedro Cruz Assistant <span className={dateTimeLabel()}>{dateTime}</span></p>
+          </div>
+          { shouldShowMessage && ( 
+            isLoading ? (
+              <div className={botMessage()}>
+                <Image src="/loading.gif" width={36} height={8} alt="loading message" />
               </div>
+            ) : (
+              <>
               <div className={botMessage()}>{message}</div>
-            </div>
-          )
-        })}
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+              { options && (
+                <div  className={buttonGroup()}>
+                  {options?.map(({ value, label, trigger }) => (
+                    <Button  key={value} variant="outline" onClick={() => handleButtonClick(trigger)}>
+                      {label}
+                    </Button>
+                  ))}
+                </div>
+              )}
+              </>
+            ))}
+        </div>
+      )
+    })
+  )
+
+ 
+  return (
+      <Card className={card()}>
+        <CardHeader className={cardHeader()}>
+          <Avatar>
+            <AvatarImage src="/me.png" alt="chatbotimg" />
+            <AvatarFallback>PC</AvatarFallback>
+          </Avatar>
+          <div>
+            <CardTitle className={cardTitle()}>Pedro Cruz Assistant</CardTitle>
+          </div>
+          <Button
+            variant="outline"
+            className={closeBtn()}
+            onClick={() => setOpen(false)}
+          >
+            <MdClose className={closeBtnIcon()} />
+          </Button>
+        </CardHeader>
+        <CardContent className={cardContent()}>
+          {renderMessages()}
+        </CardContent>
+      </Card>
   )
 }
--- components/ChatBot/styles.tsx
+++ components/ChatBot/styles.tsx
@@ -2,20 +2,21 @@ import { tv } from "tailwind-variants"
 
 const chatbot = tv({
   slots: {
-    card: "border-0 shadow-none p-0",
-    cardHeader: "flex-row items-center gap-2 border-b-2 border-black",
+    card: "relative shadow-none p-0 max-w-[500px] bg-white dark:bg-black rounded-sm border-2 border-black dark:border-white overflow-hidden",
+    cardHeader: "flex-row items-center gap-2 border-b-2 border-black dark:border-white",
     cardTitle: "text-sm font-bold",
-    closeBtn: "ml-auto mt-0 h-auto mr-0 border-2 rounded-sm p-2 border-black",
-    closeBtnIcon: "w-4 h-4",
-    cardContent: "relative flex flex-col gap-2 h-[500px] overflow-y-auto",
+    closeBtn: "ml-auto mt-0 h-auto mr-0 p-2",
+    closeBtnIcon: "w-4 h-4 dark:text-white text-black",
+    cardContent: "relative flex flex-col gap-2 h-[500px] overflow-y-auto py-8",
     dateTimeLabel:
-      "text-sm bg-black text-white px-4 py-2 mt-4 inline-block mx-auto font-semibold",
+      "text-sm font-light text-xs text-black dark:text-white",
     botContentMessage: "flex flex-col items-start",
     botName: "flex items-start text-xs font-bold gap-4",
     botImageContainer: "inline-block order-none",
     botImage: "w-10 h-10",
     botMessage:
-      "ml-14 bg-primary-500 shadow text-black p-3 relative overflow-hidden mb-2 shadow-lg text-sm font-semibold",
+      "-mt-4 ml-14 bg-lightBg dark:bg-white dark:bg-opacity-25 text-black dark:text-white p-3 relative overflow-hidden mb-2 text-sm font-semibold before:content-[attr(before)] before:w-0 before:h-0 before:border-8 before:border-solid before:border-transparent before:border-t-white dark:before:border-t-black before:border-l-white dark:before:border-l-black before:absolute before:top-0 before:left-0",
+    buttonGroup: "flex flex-col gap-2 items-end w-full mb-2",
   },
 })
 
