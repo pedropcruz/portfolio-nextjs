db245758ea632ae1332b3ca6ba69ddde0aaf50e7
Pedro Cruz <pedropcruz@outlook.pt>
2023-09-04 20:26:39 +0100

    feat(Components): Add shadcn-ui components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 px-4 py-2 shadow-none transition-shadow hover:shadow-sm-primary",
+  "inline-flex items-center justify-center rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 px-4 py-2 shadow-none transition-shadow hover:shadow-sm-primary-light dark:hover:shadow-sm-primary-dark",
   {
     variants: {
       variant: {
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-2 border-black dark:border-white bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary-500 text-secondary-foreground hover:bg-secondary-500/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
